cdaab2d9e3452eb8a0c5c0fd1dab38a9b719587f
Sarah-M94 <146059567+Sarah-M94@users.noreply.github.com>
2023-11-29 16:19:15 +0100

    push4

--- SlnLes01VisualStudioEnWpf/ConsoleMahjong/Program.cs
+++ SlnLes01VisualStudioEnWpf/ConsoleMahjong/Program.cs
@@ -10,6 +10,36 @@ namespace ConsoleMahjong
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"********************
+|Welkom Bij Mahjong|
+********************");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("druk op een");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" om de steen te tonen...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ◉◉◉ ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ◉◉◉ ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" ◉◉◉ ");
+            Console.ResetColor();
+            Console.WriteLine("druk op een toets om af te sluiten...");
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
