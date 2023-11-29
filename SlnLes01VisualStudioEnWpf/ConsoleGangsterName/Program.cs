35cc033dcaa4ff12f2d1e7074a35ebf0ca3db3ff
Sarah-M94 <146059567+Sarah-M94@users.noreply.github.com>
2023-11-29 15:22:04 +0100

    push 2

---
+++ SlnLes01VisualStudioEnWpf/ConsoleGangsterName/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGangsterName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"**********************
+|Gangsta Name Builder|
+**********************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Give the first name of any Disney character: ");
+            String disneycharacterName = Console.ReadLine();
+            
+            Console.Write("Give any workbench tool: ");
+            String workbenchTool = Console.ReadLine();
+            
+            Console.Write("What is your last name: ");
+            String lastName = Console.ReadLine();
+            Console.WriteLine();
+
+
+
+            Console.Write("Your Gangsta name: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{disneycharacterName} '{workbenchTool}' {lastName}");
+            
+            Console.ReadLine();
+        }
+    }
+}
