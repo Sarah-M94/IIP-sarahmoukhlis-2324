460d228e6f41eb553b14d522ae99d92765f1d420
Sarah-M94 <146059567+Sarah-M94@users.noreply.github.com>
2023-12-05 22:36:57 +0100

    push7

---
+++ SlnLes02VariabelenBerekeningen/ConsoleBmi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.write("lengte (in cm)");
+            float lengteCm = 
+            Console.Write("Gewicht (in KG)"); 
+
+        }
+    }
+}C
